2400ccd1e06854b37949e9d2a489bcb9b57ae716
harshithgande <156864163+harshithgande@users.noreply.github.com>
2025-03-15 15:26:25 -0700

    Commit changes that weren’t showing before

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,16 +3,20 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  // // Base path MUST match GitHub repo name unless using a custom domain
+  // base: "/SoccerForChange/",
+
   plugins: [react()],
+
   preview: {
     port: 5174,
     strictPort: true,
   },
+
   server: {
-    port: 5174,
+    port: 3000,
     strictPort: true,
     host: true,
-    origin: "http://0.0.0.0:80",
+    // origin: "http://0.0.0.0:80",
   },
 });
